5d6f1f9cc3b3418b44c8ea1866f77f683ec71e91
unknown <hasanur23910@gmail.com>
2026-03-02 14:35:35 +0600

    icon changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import { VitePWA} from 'vite-plugin-pwa';
+import { defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
